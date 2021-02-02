e42110503fdc1a0f2f51206f4d3d9248303860f3
dpd97 <danielperahia.d@gmail.com>
2021-02-02 21:29:35 +0000

    TCP connnection with Unity
    Sending lat/lon/alt to unity over TCP
    Currently just in the main script to get functionality then will make functions
    Working on GPS to cartesian conversion

--- Drone.py
+++ Drone.py
@@ -3,6 +3,7 @@ from dronekit import connect
 from dronekit import VehicleMode
 import time
 import math
+import socket
 
 
 class Drone:
@@ -11,6 +12,22 @@ class Drone:
             self.vehicle = connect(connection_str)
         except dk.APIException:
             print("Timeout")
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def bind_server_socket(self, host_ip, port):
+        host_name = socket.gethostname() # gets name of host machine
+        host_ip = socket.gethostbyname(host_name) # gets IP of host machine
+        port = 1234
+        server_address = (host_ip, port)
+        self.server_socket.bind(server_address) # binds to socket
+        self.server_socket.listen(5) # listens for TCP connections
+        print("[INFO] NOW LISTENING AT HOST ADDRESS: ", server_address)
+
+    def listen_for_tcp(self):
+        self.server_socket.listen(5)
+        print("[INFO] NOW LISTENING AT: ", server_address)
+        client_socket, client_address = self.server_socket.accept()
+        print("[INFO] GOT CONNECTION FROM: ", client_address)
 
     def arm_and_takeoff(self, aTargetAltitude):
         """
--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from Drone import Drone
 from data_logging import DataLogging
 import time
+import socket
 
 Hex = Drone("127.0.0.1:14550")
 
+# create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print('[INFO] HOST IP: ', host_ip)
+port = 5599
+server_address = (host_ip, port)
+# bind socket
+# server_socket.bind(('localhost', 1234))
+server_socket.bind(server_address)
+# socket listen
+server_socket.listen(5)
+print("[INFO] NOW LISTENING AT: ", server_address)
+
+while True:
+    client_socket, address = server_socket.accept()
+    print("[INFO] GOT CONNECTION FROM: ", address)
+    if client_socket:
+        break
+
+client_socket.send(bytes('welcome to the SITL', "utf-8"))
+
 lats = [-35.36265179, -35.36266860, -35.36309214, -35.36355729, -35.36354127]
 longs = [149.16401228, 149.16345636, 149.16293594, 149.16460797, 149.16399818]
-
 
 alt = 4
 airspeed = 5
@@ -20,6 +42,11 @@ InFlightLogging.PrepLogging()
 InFlightLogging.InfoLogging(Hex)
 
 Hex.arm_and_takeoff(alt)
+lat = Hex.get_current_location().lat
+lon = Hex.get_current_location().lon
+alt = Hex.get_current_location().alt
+client_socket.send(bytes(f'{lat},{lon},{alt}', "utf-8"))
+
 
 for i in range(plant_count):
     plant_location = Hex.get_plant_location(lats[i], longs[i], alt)
@@ -32,6 +59,12 @@ for i in range(plant_count):
         distance = Hex.distance_to_point_m(plant_location)
         print(distance)
         InFlightLogging.InfoLogging(Hex)
+
+        lat = Hex.get_current_location().lat
+        lon = Hex.get_current_location().lon
+        alt = Hex.get_current_location().alt
+
+        client_socket.send(bytes(f'{lat},{lon},{alt}', "utf-8"))
 
     plant_flag = Hex.set_plant_flag()
     print("plant flag:", plant_flag)
